96eff1997f4fb701c780530b69e253fa035832ee
C5m7b4 <mbedingfield@dcrpos.com>
2021-11-19 13:32:20 -0600

    adds tests for formatDate and isValidDate

--- src/index.js
+++ src/index.js
@@ -19,4 +19,63 @@ const isValid = (val) => {
   return true;
 };
 
-module.exports = { isValid };
+const isValidDate = (val) => {
+  if (typeof val === "undefined" || val === null) {
+    return false;
+  }
+  if (typeof val === "string" && val.length === 0) {
+    return false;
+  }
+
+  if (Object.prototype.toString.call(val) === "[object Date]") {
+    if (isNaN(val.getTime())) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  if (typeof val === "string") {
+    const newDate = new Date(val);
+    if (Object.prototype.toString.call(newDate) === "[object Date]") {
+      if (isNaN(newDate.getTime())) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+const formatDate = (date) => {
+  if (typeof date === "undefined" || date === null) return "";
+  if (typeof date === "string") {
+    if (date.length === 0) return "";
+  }
+
+  if (typeof date === "string") {
+    const newDate = new Date(date);
+    const month = newDate.getMonth() + 1,
+      day = newDate.getDate(),
+      year = newDate.getFullYear();
+
+    return month + "/" + day + "/" + year;
+  }
+
+  if (Object.prototype.toString.call(date) === "[object Date]") {
+    const month = date.getMonth() + 1,
+      day = date.getDate(),
+      year = date.getFullYear();
+
+    return month + "/" + day + "/" + year;
+  }
+
+  return "";
+};
+
+const stringDate = "1/1/2021 5:00 pm";
+// const newDate = new Date(stringDate);
+console.log(formatDate(stringDate));
+module.exports = { isValid, isValidDate, formatDate };
--- src/library.test.js
+++ src/library.test.js
@@ -1,8 +1,76 @@
-const { isValid } = require("./index");
+/* eslint-disable no-undef */
+const { isValid, isValidDate, formatDate } = require("./index");
 var assert = require("assert");
 
 describe("isValid", () => {
   it("should return false when an empty valid is present", () => {
     assert.equal(isValid(""), false);
+  });
+  it("should return false when null value is present", () => {
+    assert.equal(isValid(null), false);
+  });
+  it("should return false when an undefined value is passed in", () => {
+    assert.equal(isValid(undefined), false);
+  });
+  it("should return false when an object with no keys is passed in", () => {
+    const myObj = {};
+    assert.equal(isValid(myObj), false);
+  });
+
+  it("should return true when a valid string is passed in", () => {
+    assert.equal(isValid("Hello"), true);
+  });
+  it("should return true when an object with keys is passed in", () => {
+    const myObj = { name: "Mike", age: 47 };
+    assert.equal(isValid(myObj), true);
+  });
+});
+
+describe("isValidDate", () => {
+  it("should return false when an empty valid is present", () => {
+    assert.equal(isValidDate(""), false);
+  });
+  it("should return false when null value is present", () => {
+    assert.equal(isValidDate(null), false);
+  });
+  it("should return false when an undefined value is passed in", () => {
+    assert.equal(isValidDate(undefined), false);
+  });
+  it("should return false when an invalid Date object is passed in", () => {
+    const stringDate = "1/35/2021";
+    const newDate = new Date(stringDate);
+    assert.equal(isValidDate(newDate), false);
+  });
+  it("should return true when a new Date() object is passed in", () => {
+    const newDate = new Date();
+    assert.equal(isValidDate(newDate), true);
+  });
+  it("should return true when a string representation of a date is passed in", () => {
+    assert.equal(isValidDate("1/1/2021"), true);
+  });
+});
+
+describe("formatDate", () => {
+  it("should return an empty string when undefinded value is specified", () => {
+    assert.equal(formatDate(undefined), "");
+  });
+  it("should return an empty string when a null value is specified", () => {
+    assert.equal(formatDate(null), "");
+  });
+  it("should return an empty string if an empty string is passed in", () => {
+    assert.equal(formatDate(""), "");
+  });
+  it("should return an empty string if an object is passed in", () => {
+    const myObj = { name: "mike", age: "47" };
+    assert.equal(formatDate(myObj), "");
+  });
+  it("should return a formatted date if a string date is passed in", () => {
+    const stringDate = "1/1/2021 5:00 pm";
+    assert.equal(formatDate(stringDate), "1/1/2021");
+  });
+  it("should return a formatted date if a date object is passed in with the time", () => {
+    const stringDate = "1/1/2021 5:00 pm";
+    const newDate = new Date(stringDate);
+    assert.equal(formatDate(newDate), "1/1/2021");
   });
 });
